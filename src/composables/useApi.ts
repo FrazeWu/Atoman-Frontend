4ba9b945156f909c40b5281bee3d1ec7c336becd
FrazeWu <fz.wu@icloud.com>
2026-08-26 05:53:23 +0200

    feat(blog): use recommendation modes for home stream

--- src/composables/useApi.ts
+++ src/composables/useApi.ts
@@ -129,6 +129,7 @@ export function useApi() {
 			collection: (id: number | string) => `${apiUrl}/blog/collections/${id}`,
 
 			posts: `${apiUrl}/blog/posts`,
+			recommendPosts: `${apiUrl}/blog/recommend/posts`,
 			post: (id: number | string) => `${apiUrl}/blog/posts/${id}`,
 			postRating: (id: number | string) => `${apiUrl}/blog/posts/${id}/rating`,
 			postPublish: (id: number | string) => `${apiUrl}/blog/posts/${id}/publish`,
